49209a2c09e8c5e3ad2decf759cfd4b49af95c1f
Tom Pires Cruz <tompirescruz@macbook-pro-de-tom.home>
2021-04-28 23:31:43 +0200

    header start

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar'
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <NavBar />
+        </div>
+    );
+};
+
+export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
+import Header from '../components/Header'
 
 const Home = () => {
     return (
         <div className="Home">
-            <NavBar />
+            <Header />
         </div>
     );
 };
