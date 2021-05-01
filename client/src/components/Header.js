71e3620489aa762c66a0c498b7376ccedf9e0a0e
Tom Pires Cruz <tompirescruz@macbook-pro-de-tom.home>
2021-05-02 00:56:09 +0200

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import NavBar from './NavBar'
+import downArrow from '../assets/arrow-down.svg'
 
 const Header = () => {
     return (
-        <div className="Header">
-            <NavBar />
+        <div className="header">
+            <div className="header-content">
+                <h1>PORT<br />FOL<br />IO</h1>
+                <div className='text'>
+                    <p>Bienvenue sur mon porfolio professionnel. Vous y trouverez tous mes travaux </p>
+                    <button className='btn-alt-secondary'><a>Contactez moi</a></button>
+                </div>
+            </div>
+            <img className="down-arrow" src={downArrow} alt=""></img>
+
         </div>
     );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header'
+import NavBar from '../components/NavBar';
 
 const Home = () => {
     return (
         <div className="Home">
+            <NavBar />
             <Header />
         </div>
     );
